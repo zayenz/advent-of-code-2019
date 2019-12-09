aec125ce00332d3d02fc38b30541549819598cd1
Mikael Zayenz Lagerkvist <zayenz@gmail.com>
2019-12-09 09:37:41 +0100

    Solve day 9

---
+++ day9-1/src/main.rs
@@ -0,0 +1,67 @@
+#![allow(dead_code, unused_imports, clippy::ptr_arg)]
+
+use failure::bail;
+use failure::err_msg;
+use failure::Error;
+use itertools::*;
+use rayon::prelude::*;
+use strum_macros::EnumString;
+
+use std::char;
+use std::cmp::{max, min};
+use std::collections::{HashMap, HashSet, VecDeque};
+use std::fmt;
+use std::io::BufRead;
+use std::ops::*;
+use std::str;
+use std::str::FromStr;
+use std::{io, process};
+
+use aoc2019::input::get_numbers;
+use intcode::*;
+
+type Input = Vec<Word>;
+type Output = Word;
+
+fn read_input() -> Result<Input, Error> {
+    let stdin = io::stdin();
+    let mut result = Vec::new();
+    for line in stdin.lock().lines() {
+        let line = line?;
+        let mut numbers = get_numbers::<Word>(&line)?;
+        result.append(&mut numbers);
+    }
+
+    Ok(result)
+}
+
+fn solve(code: &Input) -> Result<Output, Error> {
+    let mut program = IntCode::new(code);
+    let mut input = VecDeque::new();
+    input.push_back(1);
+    let status = program.run(&mut input)?;
+    assert_eq!(status, Status::Done);
+    Ok(program.pop_output()?)
+}
+
+fn run() -> Result<(), Error> {
+    let input = read_input()?;
+
+    let output = solve(&input)?;
+
+    println!("{}", output);
+    Ok(())
+}
+
+fn main() {
+    match run() {
+        Ok(()) => process::exit(0),
+        Err(error) => {
+            eprintln!("Error while solving problem: {}", error);
+            for cause in error.iter_causes() {
+                eprintln!("{}", cause)
+            }
+            process::exit(1)
+        }
+    }
+}
